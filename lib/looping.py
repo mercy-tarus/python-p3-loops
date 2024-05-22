f15aa11fd2e0a7ec6ffe3eebf96575de3f70def7
mercy-tarus <merciechelah1@gmail.com>
2024-05-22 22:54:46 +0300

    complete

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,41 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+
+ counter=10
+ while counter>0:
+    print(counter)
+    counter -=1
+    print("Happy New Year!")
+    
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+   return list(map(lambda num: num**2,int_list))
+
+def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+def reverse_string(input_str):
+    reversed_str = ""
+    for char in input_str:
+        reversed_str = char + reversed_str
+    return reversed_str
